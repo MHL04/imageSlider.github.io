930e056d640c7f9ac98fa3c46d5f78dd3d75649c
Lamour Hurdell <lamourmaxime_09@icloud.com>
2022-11-10 17:04:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
- const nextEl=document.querySelector(".next");
+/*recuperation des element Html */
+const nextEl=document.querySelector(".next");
 const imageContainerEl=document.querySelector(".image-container");
-const imgEl=document.querySelectorAll("img")
-const prevEl=document.querySelector(".prev")
+const imgEl=document.querySelectorAll("img");
+const prevEl=document.querySelector(".prev");
 
 let currentImg= 1 ;
 
+let timeout;
 
-console.log(imgEl)
-
+/* entendre le click sur les elements << >> avec la fx updateImg */
  nextEl.addEventListener("click", () => {
     currentImg++;
+    clearTimeout(timeout);
     updateImg();
 
  });
  prevEl.addEventListener("click", () =>{
     currentImg--;
+    clearTimeout(timeout);
     updateImg();
 });
-
+/*fx updateImg, */
  let updateImg=() =>{
-    if(currentImg > imgEl.lenght){
-        currentImg=1;
-    }
-     else if(currentImg < 1){
-        currentImg=imgEl.lenght;
-    }
-// imageContainerEl.style.transform=`translateX(-${currentImg -1 * 500}px)`;
-    // imageContainerEl.style.transform=`translateX(-${currentImg- 1 *500}px)`;
+   if(currentImg>imgEl.length){
+    currentImg=1;
+   }else if(currentImg<1){
+    currentImg=imgEl.length;
+   }
+   //la propriete transfrom=translate permet de voir lautre image precendent
     imageContainerEl.style.transform=`translateX(-${( currentImg - 1) * 500}px)`;
+timeout =setTimeout(()=>{
+    currentImg++;
+    updateImg();
+},3000);
 
  }
